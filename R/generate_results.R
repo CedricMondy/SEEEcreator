0dc2df1fabb0809ecb8d256a933198300ed1afdd
Cédric Mondy <cedric.mondy@gmail.com>
2018-02-05 13:34:05 +0100

    include less strict rules to locate the arguments definition in the calc script

--- R/generate_results.R
+++ R/generate_results.R
@@ -1,7 +1,7 @@
 generate_results <- function(calc, inputFiles) {
   calc_content <- readLines(con = calc, skipNul = FALSE)
 
-  argsLoc <- which(grepl("arguments <- ", calc_content))
+  argsLoc <- which(grepl("arguments.*<-.*commandArgs", calc_content))
   calc_content[argsLoc] <- paste0('arguments <- c("',
                                   paste(inputFiles, collapse = '", "'),
                                   '")')
